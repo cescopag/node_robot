d3b8d44d8258766e2b7d48771ac53449a7d47ffe
Francesco Paggin <francesco.paggin@dianacorp.com>
2015-11-13 12:57:20 +0100

    ping

--- robot.js
+++ robot.js
@@ -1,69 +1,94 @@
-'use strict';
 var five = require('johnny-five');
 var board = new five.Board({ port: "/dev/rfcomm0" });
 var hapi = require('hapi');
 board.on('ready', start);
 var motorA, motorB;
+var air = 0;
 
 var server = new hapi.Server();
 server.connection({
-	host: 'localhost',
 	port: 8080
 });
 
-server.route({
-	method: 'GET',
-	path: '/forward',
-	handler: function(req,reply) {
-		console.log('forward');
-		motorA && motorA.forward(255);
-		motorB && motorB.forward(255);
-		reply(200);
-	}
-});
+server.register(require('inert'), function() {
 
-server.route({
-	method: 'GET',
-	path: '/left',
-	handler: function(req,reply) {
-		console.log('left');
-		motorA && motorA.reverse(128);
-		motorB && motorB.forward(128);
-		reply(200);
-	}
-});
+	server.route({
+		path: '/',
+	    handler: function (request, reply) {
+	        reply.file('index.html');
+	    }
+	});
 
-server.route({
-	method: 'GET',
-	path: '/right',
-	handler: function(req,reply) {
-		console.log('right');
-		motorA && motorA.forward(128);
-		motorB && motorB.reverse(128);
-		reply(200);
-	}
-});
+	server.route({
+		method: 'POST',
+		path: '/forward',
+		handler: function(req,reply) {
+			console.log('forward');
+			motorA && motorA.forward(255);
+			motorB && motorB.forward(255);
+			reply(200);
+		}
+	});
 
-server.route({
-	method: 'GET',
-	path: '/reverse',
-	handler: function(req,reply) {
-		console.log('reverse');
-		motorA && motorA.reverse(255);
-		motorB && motorB.reverse(255);
-		reply(200);
-	}
-});
+	server.route({
+		method: 'POST',
+		path: '/left',
+		handler: function(req,reply) {
+			console.log('left');
+			motorA && motorA.reverse(128);
+			motorB && motorB.forward(128);
+			reply(200);
+		}
+	});
 
-server.route({
-	method: 'GET',
-	path: '/stop',
-	handler: function(req,reply) {
-		console.log('stop');
-		motorA && motorA.stop(0);
-		motorB && motorB.stop(0);
-		reply(200);
-	}
+	server.route({
+		method: 'POST',
+		path: '/right',
+		handler: function(req,reply) {
+			console.log('right');
+			motorA && motorA.forward(128);
+			motorB && motorB.reverse(128);
+			reply(200);
+		}
+	});
+
+	server.route({
+		method: 'POST',
+		path: '/reverse',
+		handler: function(req,reply) {
+			console.log('reverse');
+			motorA && motorA.reverse(255);
+			motorB && motorB.reverse(255);
+			reply(200);
+		}
+	});
+
+	server.route({
+		method: 'POST',
+		path: '/stop',
+		handler: function(req,reply) {
+			console.log('stop');
+			motorA && motorA.stop(0);
+			motorB && motorB.stop(0);
+			reply(200);
+		}
+	});
+
+	server.route({
+		method: 'GET',
+		path: '/radar',
+		handler: function(req,reply) {
+			reply(air);
+		}
+	});
+
+	server.start(function(err){
+	    if (err) {
+	        throw err;
+	    }
+	    console.log('Server running at:', server.info.uri);
+	});
+
 });
 
 function start() {
@@ -85,12 +110,14 @@ function start() {
 			cdir: 4
 		}
 	});
-}
 
-server.start(function(err){
-    if (err) {
-        throw err;
-    }
-    console.log('Server running at:', server.info.uri);
-});
+	new five.Proximity({
+  		controller: "PING_PULSE_IN",
+  		pin: "A0"
+	});
+
+	proximity.on("data", function() {
+    	var air = this.cm;
+  	});
+}
 

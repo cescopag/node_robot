5d93af998ed6c14b67becd5aa0a7027bd1092708
cescopag <cescopag@gmail.com>
2015-11-16 21:54:37 +0100

    Fix boot sequence

--- start.sh
+++ start.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 echo "Connecting bluetooth"
 sudo rfcomm release 0
-sudo rfcomm bind 0 98:D3:31:40:04:6B
-rfcomm show 0
+sudo rfcomm connect 0 98:D3:31:40:04:6B &
 echo "Network interface:"
 ifconfig wlan0
-sleep 10
+sleep 15
 echo "Connecting robot"
 sudo node robot.js

3c12d0365a5d6dd2ebe3b6a3e61d7ca1007890c3
Francesco Paggin <francesco.paggin@dianacorp.com>
2015-11-17 09:16:30 +0100

    clean up

--- readme.txt
+++
@@ -1,8 +0,0 @@
-# collegare bluetooth
-sudo rfcomm bind 0 98:D3:31:40:04:6B
-
-# verificare stato bluetooth
-frcomm show 0
-
-# disconnettere bluetooth
-sudo rfcomm release 0
--- start.sh
+++ start.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-echo "Connecting bluetooth"
+echo "Connecting bluetooth ..."
 sudo rfcomm release 0
 sudo rfcomm connect 0 98:D3:31:40:04:6B &
-echo "Network interface:"
+echo "Network interface ..."
 ifconfig wlan0
-sleep 15
-echo "Connecting robot"
+sleep 10
+echo "Connecting robot ..."
 sudo node robot.js
